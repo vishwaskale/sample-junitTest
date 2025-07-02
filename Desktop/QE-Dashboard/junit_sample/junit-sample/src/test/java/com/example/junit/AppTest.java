e246f4f3625ec8507e1e5dedfcd220f79dd4d947
Vishwas <vishwas.30kale@gmail.com>
2025-07-02 20:15:20 +0530

    Initial commit with JUnit 5, Surefire, and SonarCloud config

---
+++ Desktop/QE-Dashboard/junit_sample/junit-sample/src/test/java/com/example/junit/AppTest.java
@@ -0,0 +1,18 @@
+package com.example.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
